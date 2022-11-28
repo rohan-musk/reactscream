4ecef8b6c9619dca26e30f41a9594019413a7a2c
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2022-11-29 00:19:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import Data from './data/data.json'
+
+const Mapper = ({ data }) => {
+  const [show, setShow] = useState(-1)
+
+
+  return (
+    <div className="container" >
+      {data.map((item, index) => {
+        return (
+          <div key={item.topic.hash}>
+            <button onClick={() => { setShow(index) }}><a href={item.topic.hash} >{item.topic.title}</a></button>
+            {index === show &&
+              item.childrens && <Mapper data={item.childrens} />
+
+            }
+          </div>
+        )
+
+      })}
+    </div>
+  )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><a href="/">Scream Home</a></h1>
+      <Mapper data={Data} />
     </div>
   );
 }
